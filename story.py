8399bf65a8b59fdf62d0462bdda6bad03bd7038c
dirkwind <dirkwind707@gmail.com>
2020-09-19 21:54:24 -0500

    move enemy dict formatting to enemy_format.txt

--- story.py
+++ story.py
@@ -5,46 +5,7 @@ from aux_functions import *
 from playsound import *
 from time import *
 
-'''
-# standard enemy dictionary
-enemy = {
-    'name': 'name',
-    'intro': '', # intro message
-    'description': '', # short description of enemy
-    'checkable': [True, "name can't be checked."], # whether or not player can check enemy's stats
-    'attack': 0,
-    'defense': 0,
-    'speed': 0, # 4+ = no Great Hits, 8+ = no Good Hits, 13+ = no Fair Hits, 20+ = 100% immune to damage items, all keep Critical (nums in b/w included)
-    'health': 0,
-    'max_health': 0,
-    'give_up': [0, ""],
-    'can_flee': True,
-    'bonus_xp': 0, # extra xp for the player if they defeat this enemy
-    'bonus_attack': 0, # used for keeping track of attack bonus from items
-    'bonus_defense': 0,
-    'inventory': [ # Enemy_Item(name='Item', purpose='stat_change', 'heal', 'damage', or 'special', value=0, uses=1, dodgable=None, turns=0, changed_stat='', special_func=None, special_params=[], silent=False)
-
-    ],
-    'use_item_chance': 0, # between and including 1 through 100; percentage chance (w/ precision of 3 decimal places) that the enemy will use an item
-    'responses': { # use (['', ''], 0, stat, value) to change a stat's value when an act is performed
-        '': [
-            (['', ''], 0)  
-            ],
-        '': [
-            (['', ''], 0)
-            ],
-        '': [
-            (['', ''], 0)
-            ],
-        '': [
-            (['', ''], 0)
-            ]
-    }
-}
-
-item format: [purpose, name, value, turns/dodgable/params, uses]
-
-'''
+# check enemy_format.txt for enemy formatting info
 
 dog = {
     'name': 'Gremlin',
@@ -113,6 +74,10 @@ doge = {
             ]
     }
 }
+
+doge = Enemy.from_dict(doge)
+
+print(doge.inventory)
 
 #add_item(Player_Item('Anaesthetic Bomb', 'special', None, 2, special_func=ana_bomb, special_params=['$enemy', 1]))
 #add_item(Player_Item('Effect Longevity Boost', 'special', None, 3, special_func=change_turns, special_params=['$turns', -4, '&turns']))
